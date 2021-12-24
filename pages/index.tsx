d639e7a8a3a9d277ec1fac2eb2ad366f24f808aa
Shane O'Moore <shaneiadt@gmail.com>
2021-12-24 10:32:48 +0000

    fetch json config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,20 @@
-import React from 'react';
-import { useDeno } from 'https://deno.land/x/aleph/framework/react/mod.ts';
-import { Marked } from 'https://deno.land/x/markdown@v2.0.0/mod.ts';
-
-const getPages = async (dir: string) => {
-  const pages = [];
-
-  for await (const page of Deno.readDir(dir)) {
-    pages.push(`${dir}/${page.name}`);
-  }
-
-  return pages;
-}
+import React, { useEffect, useState } from 'react';
 
 export default () => {
-  const articles = useDeno(async () => {
-    const tree = [];
-    const meta = [];
+  const [content, setArticles] = useState<{ articles: { href: string, title: string, date: string, tags: string[], summary: string }[] }>({ articles: [] });
 
-    for await (const dirEntry of Deno.readDir('./pages')) {
-      if (dirEntry.isDirectory) {
-        const pages = await getPages(`./pages/${dirEntry.name}`);
-
-        for (const page of pages) {
-          tree.push(page);
-        }
-      }
-    }
-
-    for await (const filename of tree) {
-      const decoder = new TextDecoder("utf-8");
-      const markdown = decoder.decode(await Deno.readFile(filename));
-      const markup = Marked.parse(markdown);
-      const summary = markup.content.slice(0, 500);
-      const { title, date, categories, tags } = markup.meta;
-
-      meta.push({ href: filename.replace('.md', '').replace('./pages/', ''), title, date, categories, tags, summary });
-    }
-
-    meta.sort((a, b) => b.date - a.date);
-
-    return meta;
-  });
+  useEffect(() => {
+    fetch(`${window.location.origin}/articles.json`)
+      .then(response => response.json())
+      .then(data => {
+        setArticles(data);
+      })
+      .catch(e => console.error(e));
+  }, []);
 
   return (
     <div className="page">
-      {articles.map(article => (
+      {content.articles.map((article) => (
         <div key={article.href} className="blog-post-content d-flex align-items-center flex-wrap margin-60px-bottom padding-60px-bottom border-bottom border-color-extra-light-gray md-margin-30px-bottom md-padding-30px-bottom text-center text-md-start md-no-border">
           <div className="col-12 col-lg-5 blog-image p-0 md-margin-30px-bottom sm-margin-20px-bottom margin-45px-right md-no-margin-right">
             <a href={article.href}><img src="https://via.placeholder.com/1200x840" alt={article.title} /></a>
@@ -53,7 +23,7 @@ export default () => {
             <div className="content margin-20px-bottom md-no-padding-left ">
               <a href={article.href} className="text-extra-dark-gray margin-5px-bottom alt-font text-extra-large font-weight-600 d-inline-block">{article.title}</a>
               <div className="text-medium-gray text-extra-small margin-15px-bottom text-uppercase alt-font"><span>{new Date(article.date).toDateString()}</span></div>
-              <p className="m-0 w-95 lg-w-100">Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum has been the industry industry’s standard dummy text Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              <p className="m-0 w-95 lg-w-100" dangerouslySetInnerHTML={{ __html: article.summary }}></p>
               <div className="text-medium-gray text-extra-small margin-15px-bottom text-uppercase alt-font mt-2">
                 {article.tags.map((tag: string) => (
                   <span key={tag} style={{ color: '#fff', display: 'inline-block', padding: '5px', backgroundColor: 'lightseagreen', borderRadius: '3px', marginRight: '5px' }}>{tag}</span>
